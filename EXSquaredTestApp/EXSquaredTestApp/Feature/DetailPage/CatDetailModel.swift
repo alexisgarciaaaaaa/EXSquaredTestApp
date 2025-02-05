9c40b1644be4e8e6941b34b70534b38b6b946222
bryangarcia <alexisgarciathb1996@gmail.com>
2025-02-05 16:03:11 -0600

    chore(cleanup): remove unused variables from cat detail response

--- EXSquaredTestApp/EXSquaredTestApp/Feature/DetailPage/CatDetailModel.swift
+++ EXSquaredTestApp/EXSquaredTestApp/Feature/DetailPage/CatDetailModel.swift
@@ -10,56 +10,25 @@ import SwiftUI
 struct CatDetail: Codable, Identifiable {
     let id: String
     let name: String?
-    let weight: Weight
     let cfaURL: String?
     let vetstreetURL: String?
     let vcahospitalsURL: String?
     let temperament: String?
     let origin: String?
-    let countryCodes: String?
-    let countryCode: String?
     let description: String?
     let lifeSpan: String?
-    let indoor: Int?
-    let lap: Int?
-    let altNames: String?
     let adaptability: Int?
     let affectionLevel: Int?
     let childFriendly: Int?
     let dogFriendly: Int?
-    let energyLevel: Int?
-    let grooming: Int?
-    let healthIssues: Int?
     let intelligence: Int?
-    let sheddingLevel: Int?
-    let socialNeeds: Int?
-    let strangerFriendly: Int?
-    let vocalisation: Int?
-    let experimental: Int?
-    let hairless: Int?
-    let natural: Int?
-    let rare: Int?
-    let rex: Int?
-    let suppressedTail: Int?
-    let shortLegs: Int?
     let wikipediaURL: String?
-    let hypoallergenic: Int?
     let referenceImageID: String?
 
     enum CodingKeys: String, CodingKey {
-        case id, name, weight, temperament, origin, description, lifeSpan = "life_span"
-        case cfaURL = "cfa_url", vetstreetURL = "vetstreet_url", vcahospitalsURL = "vcahospitals_url"
-        case countryCodes = "country_codes", countryCode = "country_code"
-        case indoor, lap, altNames = "alt_names", adaptability, affectionLevel = "affection_level"
-        case childFriendly = "child_friendly", dogFriendly = "dog_friendly", energyLevel = "energy_level"
-        case grooming, healthIssues = "health_issues", intelligence, sheddingLevel = "shedding_level"
-        case socialNeeds = "social_needs", strangerFriendly = "stranger_friendly", vocalisation
-        case experimental, hairless, natural, rare, rex, suppressedTail = "suppressed_tail"
-        case shortLegs = "short_legs", wikipediaURL = "wikipedia_url", hypoallergenic, referenceImageID = "reference_image_id"
+        case id, name, temperament, origin, description
+        case lifeSpan = "life_span", cfaURL = "cfa_url", vetstreetURL = "vetstreet_url", vcahospitalsURL = "vcahospitals_url"
+        case adaptability, affectionLevel = "affection_level", childFriendly = "child_friendly", dogFriendly = "dog_friendly"
+        case intelligence, wikipediaURL = "wikipedia_url", referenceImageID = "reference_image_id"
     }
-}
-
-struct Weight: Codable {
-    let imperial: String
-    let metric: String
 }
