3e460ac6bc89fa2bc811a5227291c887377d5712
bryangarcia <alexisgarciathb1996@gmail.com>
2025-02-05 11:31:28 -0600

    chore(init): init project on EXSquare Test

---
+++ EXSquaredTestApp/EXSquaredTestApp/EXSquaredTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EXSquaredTestAppApp.swift
+//  EXSquaredTestApp
+//
+//  Created by bryangarcia on 5/2/25.
+//
+
+import SwiftUI
+
+@main
+struct EXSquaredTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
